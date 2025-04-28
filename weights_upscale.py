fa84537e0fdad6c79d406b8617f7c318ba826a05
mohamed-services <54558824+mohamed-services@users.noreply.github.com>
2025-04-28 04:15:51 +0300

    Create weights_upscale.py

---
+++ weights_upscale.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+
+w0 = np.load('weights/weights_0.npy')
+
+axis = 2
+shape1 = np.array([i.shape for i in w0])
+shape1[:,axis] *= 2
+shape1[axis,-1] *= 2
+shape1
+
+w1 = [np.zeros(i, dtype=w0.dtype) for i in shape1]
+for i in range(len(w0)):
+    if i != axis:
+        w1[i] = np.concatenate([w0[i],w0[i]], axis=axis)
+    else:
+        ndim = w0[i].ndim
+        shape_axis = w0[i].shape[axis]
+        shape_last = w0[i].shape[-1]
+        slice1 = [slice(None)] * ndim
+        slice2 = [slice(None)] * ndim
+        slice1[axis] = slice(None, shape_axis)
+        slice2[axis] = slice(shape_axis, None)
+        slice1[-1] = slice(None, shape_last)
+        slice2[-1] = slice(shape_last, None)
+        w1[i][tuple(slice1)] = w0[i]
+        w1[i][tuple(slice2)] = w0[i]
+
