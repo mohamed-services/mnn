6723a02545ae22b0f7fc07b2df2bbd6fc6d22b3b
mohamed-services <54558824+mohamed-services@users.noreply.github.com>
2025-05-06 21:58:01 +0300

    Update train.py

--- model/gd/train.py
+++ model/gd/train.py
@@ -5,13 +5,22 @@ import os
 import itertools
 import tensorflow as tf
 import keras
-import D:/mnn/layer.py 
+from mnn.layer import MNN  # type: ignore
 
 path = 'D:/mnn/model/gd/weights/'
 w = []
 for i in range(len(os.listdir(path))):
-    w.append(np.load(f'D:/mnn/model/ea/weights/axis_{i}.npy').astype(np.float64))
+    w.append(np.load(path+f'axis_{i}.npy').astype(np.float64))
 
 shape = [i.shape[-1] for i in w]
 shape
+
+inputs = keras.layers.Input(shape)
+layer = MNN(shape, 'tf')(inputs)
+
+model = keras.Model(inputs, layer)
+
+model.compile(keras.optimizers.SGD(2**-4), 'mse')
+
+model.summary()
 
