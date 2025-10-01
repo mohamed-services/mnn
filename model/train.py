d97b237223921cb13aa10692073bfd114e37cc28
mohamed-services <54558824+mohamed-services@users.noreply.github.com>
2025-10-01 14:34:37 +0300

    Create train.py

---
+++ model/train.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+import string
+import os
+import itertools
+import tensorflow as tf
+import keras
+from mnn.layer import MNN  # type: ignore
+
+path = 'D:/mnn/model/gd/weights/'
+w = []
+for i in range(len(os.listdir(path))):
+    w.append(np.load(path+f'axis_{i}.npy').astype(np.float64))
+
+shape = [i.shape[-1] for i in w]
+shape
+
+inputs = keras.layers.Input(shape)
+layer = MNN(shape, 'tf')(inputs)
+
+model = keras.Model(inputs, layer)
+
+model.compile(keras.optimizers.SGD(2**-2), 'mse')
+
+model.summary()
+
+def binary(size):
+    return np.array([[int(j) for j in bin(i)[2:].zfill(size)] for i in range(2**size)])
+
+size = 2
+x = binary(size) * 2 - 1
+x
+x.shape = [x.shape[0], *shape]
+
+model.evaluate(x, x)
+
+model.fit(x,x, epochs=2**16)
