6c453b4c794e481c42ea852e9def35e49a5a41a4
mohamed-services <54558824+mohamed-services@users.noreply.github.com>
2025-10-04 02:47:57 +0300

    Update train.py

--- model/train.py
+++ model/train.py
@@ -8,13 +8,18 @@ from mnn.layer import MNN # type: ignore
 path = 'D:/mnn/model/weights/'
 w = []
 for i in range(len(os.listdir(path))):
-    w.append(np.load(path+f'axis_{i}.npy').astype(np.float64))
+    w.append(np.load(path+f'axis_{i}.npy').astype(np.float32))
 
 shape = [i.shape[-1] for i in w]
 shape
 
 inputs = keras.layers.Input(shape)
-layer = MNN(shape, 'tf')(inputs)
+layer = inputs
+mnn_layer = MNN(shape, 'tf')
+relu = keras.layers.ReLU()
+layer = mnn_layer(layer)
+layer = relu(layer)
+layer = mnn_layer(layer)
 
 model = keras.Model(inputs, layer)
 
@@ -27,7 +32,7 @@ model.summary()
 def binary(size):
     return np.array([[int(j) for j in bin(i)[2:].zfill(size)] for i in range(2**size)])
 
-size = 2
+size = 4
 x = binary(size) * 2 - 1
 x
 x.shape = [x.shape[0], *shape]
@@ -35,3 +40,10 @@ x.shape = [x.shape[0], *shape]
 model.evaluate(x, x)
 
 model.fit(x,x, epochs=2**16)
+
+'''
+w = model.get_weights()
+for i in range(len(w)):
+    np.save(path+f'axis_{i}.npy', w[i])
+
+'''
