010d4fb3ecfd7b0fae75100cca75a395d9c1c030
mohamed-services <54558824+mohamed-services@users.noreply.github.com>
2025-11-04 20:19:29 +0200

    Update train.py

--- model/train.py
+++ model/train.py
@@ -17,9 +17,9 @@ shape
 weights = {axes[i]:w[i] for i in range(dims)}
 
 inputs = keras.layers.Input(shape)
-layer = inputs
 mnn_layer = MNN(shape, backend='tf', weights=weights)
 relu = keras.layers.ReLU()
+layer = inputs
 
 for i in range(7):
     layer = mnn_layer(layer)
@@ -38,8 +38,9 @@ def err_max(y_true, y_pred):
 def err_sum(y_true, y_pred):
     return tf.reduce_sum(tf.abs(tf.cast(y_true, dtype=tf.float32) - y_pred))
 
-model.compile(keras.optimizers.Adam(), 'mse', metrics=[err_sum, err_max])
-#model.compile(keras.optimizers.SGD(learning_rate=2.0**-1), 'mse', metrics=[err])
+model.compile(keras.optimizers.Adam(learning_rate=2**-10), 'mse', metrics=[err_sum, err_max])
+#model.compile(keras.optimizers.Adam(learning_rate=2**-10, weight_decay=2**-10), 'mse', metrics=[err_sum, err_max])
+#model.compile(keras.optimizers.SGD(learning_rate=2**-4, weight_decay=2**-4), 'mse', metrics=[err_sum, err_max])
 
 model.summary()
 
@@ -70,6 +71,7 @@ with open(dataset_file_name, 'r', encoding='utf-8') as f:
 
 model.evaluate([x, mask], x, batch_size=x.shape[0])
 
+model.fit([x, mask], x, epochs=2**8)
 model.fit([x, mask], x, batch_size=x.shape[0], epochs=2**16)
 
 '''
@@ -84,9 +86,16 @@ np.abs(w[0]).max()
 np.abs(w[1]).max()
 np.abs(w[2]).max()
 np.abs(w[3]).max()
+np.abs(w[0]).mean()
+np.abs(w[1]).mean()
+np.abs(w[2]).mean()
+np.abs(w[3]).mean()
 w = model.get_weights()
 np.abs(w[0]).max()
 np.abs(w[1]).max()
 np.abs(w[2]).max()
 np.abs(w[3]).max()
-
+np.abs(w[0]).mean()
+np.abs(w[1]).mean()
+np.abs(w[2]).mean()
+np.abs(w[3]).mean()
